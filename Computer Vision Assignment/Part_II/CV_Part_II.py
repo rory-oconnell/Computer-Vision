2d2fb20062e0a7bd33d9b89d9c8c7ab869e05292
oconner9 <oconner9@tcd.ie>
2023-10-20 13:53:25 +0100

    Added a function to scale the photos down

--- Computer Vision Assignment/Part_II/CV_Part_II.py
+++ Computer Vision Assignment/Part_II/CV_Part_II.py
@@ -12,6 +12,12 @@ import cv2 as cv
 import numpy as np
 import os
 
+def rescaleFrame(frame, scale):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 if __name__ == '__main__':
 
     # Directory containing the images
@@ -31,6 +37,9 @@ if __name__ == '__main__':
         if img is None:
             print(f"Error: Image {img_file} not loaded!")
             continue
+
+        # Resize the image
+        img = rescaleFrame(img, 0.25)
 
         cv.imshow('Table', img)
 
