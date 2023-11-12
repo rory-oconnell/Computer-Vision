d6a92739e6a92ea8a95576659cdedc6349286b5e
oconner9 <oconner9@tcd.ie>
2023-11-12 18:32:30 +0000

    Consolidated the project to be run from one file

--- Computer Vision Assignment/Part_II/CV_Part_II.py
+++ Computer Vision Assignment/Part_II/CV_Part_II.py
@@ -1,6 +1,6 @@
 import cv2
+import os
 import numpy as np
-from matplotlib import pyplot as plt
 from itertools import combinations
 
 # Define the angle similarity threshold
@@ -105,100 +105,115 @@ def rescaleFrame(frame, scale):
     dimensions = (width, height)
     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 
-original_image = cv2.imread("Computer Vision Assignment/Part_II/Tables/Table3.jpg")
-hsv_original = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+def bigFunc(original_image, roi):
 
-roi = cv2.imread("Computer Vision Assignment/Part_II/Tables/Table3_BPJ.png")
-hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    hsv_original = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-# Histogram ROI 
-roi_hist = cv2.calcHist([hsv_roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
-mask = cv2.calcBackProject([hsv_original], [0, 1], roi_hist, [0, 180, 0, 256], 1)
+    # Histogram ROI 
+    roi_hist = cv2.calcHist([hsv_roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    mask = cv2.calcBackProject([hsv_original], [0, 1], roi_hist, [0, 180, 0, 256], 1)
 
-# Filtering remove noise
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-mask = cv2.filter2D(mask, -1, kernel)
-_, mask = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)
+    # Filtering remove noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    mask = cv2.filter2D(mask, -1, kernel)
+    _, mask = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)
 
-mask = cv2.merge((mask, mask, mask))
-result = cv2.bitwise_and(original_image, mask)
-cv2.imshow("Result", rescaleFrame(result, 0.25))
-cv2.imshow("Mask", rescaleFrame(mask, 0.25))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    mask = cv2.merge((mask, mask, mask))
+    result = cv2.bitwise_and(original_image, mask)
 
-# Dialate the image
-kernel = np.ones((3, 3), np.uint8)
-dilation = cv2.dilate(result,kernel,iterations = 2)
+    # Dialate the image
+    kernel = np.ones((3, 3), np.uint8)
+    dilation = cv2.dilate(result,kernel,iterations = 2)
 
-# Opening and Closing
-kernel = np.ones((9, 9), np.uint8)
-for i in range(100):
-    dilation = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
-    dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+    # Opening and Closing
+    kernel = np.ones((9, 9), np.uint8)
+    for i in range(100):
+        dilation = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+        dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
 
-# Closing
-kernel = np.ones((9, 9), np.uint8)
-for i in range(100):
-    dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+    # Closing
+    kernel = np.ones((9, 9), np.uint8)
+    for i in range(100):
+        dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
 
-cv2.imshow("Mask", rescaleFrame(dilation, 0.25))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Hough Line Transform
+    edges = cv2.Canny(dilation, 100, 200)
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
 
+    # Extend the lines across the entire image
+    for line in lines:
+        rho, theta = line[0]
+        intersections = intersection_point(rho, theta, result.shape[1], result.shape[0])
+        if len(intersections) == 2:  # We need exactly two points to draw a line
+            cv2.line(result, intersections[0], intersections[1], (0, 0, 255), 2)
 
-# Hough Line Transform
-edges = cv2.Canny(dilation, 100, 200)
-lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+    # List to hold the coordinates of the intersection points
+    intersection_points = []
 
-# Extend the lines across the entire image
-for line in lines:
-    rho, theta = line[0]
-    intersections = intersection_point(rho, theta, result.shape[1], result.shape[0])
-    if len(intersections) == 2:  # We need exactly two points to draw a line
-        cv2.line(result, intersections[0], intersections[1], (0, 0, 255), 2)
+    for line1, line2 in combinations(lines, 2):
+        try:
+            # Only calculate intersection if the angles of the lines are not too similar
+            if not are_angles_similar(line1[0][1], line2[0][1]):
+                x, y = line_intersection(line1, line2)
+                if is_point_inside_image(x, y, original_image.shape):
+                    intersection_points.append((x, y))
+        except np.linalg.LinAlgError:
+            # Lines are parallel and cannot be intersected
+            continue
 
-# List to hold the coordinates of the intersection points
-intersection_points = []
+    # Mark the intersection points on the image
+    for point in intersection_points:
+        cv2.circle(result, point, radius=5, color=(0, 255, 0), thickness=-1)  # -1 thickness fills the circle
 
-for line1, line2 in combinations(lines, 2):
-    try:
-        # Only calculate intersection if the angles of the lines are not too similar
-        if not are_angles_similar(line1[0][1], line2[0][1]):
-            x, y = line_intersection(line1, line2)
-            if is_point_inside_image(x, y, original_image.shape):
-                intersection_points.append((x, y))
-    except np.linalg.LinAlgError:
-        # Lines are parallel and cannot be intersected
-        continue
+    # Assuming intersection_points is a list of tuples (x, y) of intersection points
+    largest_quad = find_largest_quadrilateral(intersection_points)
 
-# Mark the intersection points on the image
-for point in intersection_points:
-    cv2.circle(result, point, radius=5, color=(0, 255, 0), thickness=-1)  # -1 thickness fills the circle
+    # Check if a largest quad was found and print or draw it
+    if largest_quad:
+        # Convert points to float32
+        src_pts = np.array(largest_quad, dtype="float32")
+        dst_pts = np.array([[0, 0],
+                            [750, 0],
+                            [750, 750],
+                            [0, 750]], dtype="float32")
 
-# Assuming intersection_points is a list of tuples (x, y) of intersection points
-largest_quad = find_largest_quadrilateral(intersection_points)
+        # Compute the perspective transform matrix and apply it
+        M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+        warped_image = cv2.warpPerspective(original_image, M, (750, 750))
 
-# Check if a largest quad was found and print or draw it
-if largest_quad:
-    # Convert points to float32
-    src_pts = np.array(largest_quad, dtype="float32")
+        cv2.imshow("Original Image", rescaleFrame(original_image, 0.25))
+        cv2.imshow("Plan View", warped_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("No quadrilateral found.")
 
-    # Define the destination points for the "doubled" dimensions
-    # The destination image will be twice as large as the standard table size in pixels
-    # Standard table size is 274 cm x 152.5 cm, this is approximated to 400 x 600 pixels
-    dst_pts = np.array([[0, 0],
-                        [750, 0],
-                        [750, 750],
-                        [0, 750]], dtype="float32")
+if __name__ == '__main__':
+    img_dir = "Computer Vision Assignment/Part_II/Tables"
+    bpj_dir = "Computer Vision Assignment/Part_II/BPJ"
+    
+    img_files = [f for f in os.listdir(img_dir) if f.startswith('Table') and f.endswith('.jpg')]
+    bpj_files = [f for f in os.listdir(bpj_dir) if f.startswith('BPJ_Table') and f.endswith('.png')]
+    
+    img_files.sort()
+    bpj_files.sort()
 
-    # Compute the perspective transform matrix and apply it
-    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-    # The size of the output image will be 400 x 600 pixels
-    warped_image = cv2.warpPerspective(original_image, M, (800, 800))
+    for img_file, bpj_file in zip(img_files, bpj_files):
+        img_path = os.path.join(img_dir, img_file)
+        bpj_path = os.path.join(bpj_dir, bpj_file)
+        
+        original_image = cv2.imread(img_path)
+        roi = cv2.imread(bpj_path)
 
-    cv2.imshow("Plan View", warped_image)
-    cv2.waitKey(0)
+        if original_image is None or roi is None:
+            print(f"Error: Image {img_file} or BPJ {bpj_file} not loaded!")
+            continue
+        
+        bigFunc(original_image, roi)
+        
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            break
+
     cv2.destroyAllWindows()
-else:
-    print("No quadrilateral found.")
---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Run this file and cycle through each stage with the spacebar.
+
+subprocess.run(['python', 'Computer Vision Assignment\Part_I\CV_Part_I.py'])
+subprocess.run(['python', 'Computer Vision Assignment\Part_II\CV_Part_II.py'])
+subprocess.run(['python', 'Computer Vision Assignment\Part_III\CV_Part_III.py'])
