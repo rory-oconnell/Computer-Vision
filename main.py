8c03eb81ff77290d40cc7ba16f4b3a3877e70c7f
oconner9 <oconner9@tcd.ie>
2023-11-12 23:14:13 +0000

    Wrapping up

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import subprocess
 subprocess.run(['python', 'Computer Vision Assignment\Part_I\CV_Part_I.py'])
 subprocess.run(['python', 'Computer Vision Assignment\Part_II\CV_Part_II.py'])
 subprocess.run(['python', 'Computer Vision Assignment\Part_III\CV_Part_III.py'])
+
+# Note for Part II: The program will run for a while, but it will eventually finish.
+# Note for Part III: To see the terminal print out results, you must run the file in the Part II folder. Otherwise there is a copy of the output in the folder also.
